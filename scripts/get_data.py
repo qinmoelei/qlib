b1ee013f8d225935a5565521de2898b78d4e48d3
qml <937883273@qq.com>
2022-01-26 19:36:31 +0800

    adding comments to read more easily

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -3,7 +3,7 @@
 
 import fire
 from qlib.tests.data import GetData
-
+#the fire package helps us to transfer our code to generate command line interfaces
 
 if __name__ == "__main__":
     fire.Fire(GetData)
